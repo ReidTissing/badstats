85f4575ef207adabea2fc2fd249892dd3851d8a8
ReidTissing <reid.tissing@gmail.com>
2020-01-23 13:28:06 -0600

    formatting changes

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const scriptPath = "./app.py";
-
+// const d3 = require('d3');
 const { exec } = require('child_process');
-
+const csv = require('csvtojson');
 var app = require('express')();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
+
+const csvs = ['presweight','serialkillers'].map(csvname => csvname+'.csv');
+function getCsvData (csvName) {
+  return csv().fromFile(csvName);
+};
+
 
 function execPythonScript(result) {
   exec(`python ${scriptPath}`, (error, stdout, stderr) => {
@@ -32,11 +38,12 @@ app.get('/' , function(req , res) {
     res.render('index' , {result_from_database: res.body} );   
 });
 
-app.get('/stats', function(req, res) {
-  // var randint = getRandomInt(500);
-  //    res.json({
-  //      message: randint
-  //    });
+app.get('/stats', async function(req, res) {
+//get csv data to work with
+  const serialkillers = await getCsvData('serialkillers.csv');
+  // console.log(JSON.stringify(csvdata))
+  //output array elements from csv
+  console.log(serialkillers[1].name);
   execPythonScript((output, error) => {
     if (error) {
       res.status(500).send({ error: `Something fucking went wrong. ${error}` })
